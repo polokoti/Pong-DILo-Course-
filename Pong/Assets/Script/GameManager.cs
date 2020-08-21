af78f5caaecea7e413afec41a90b5bf953490f98
polokoti <63522828+polokoti@users.noreply.github.com>
2020-08-21 22:01:11 +0700

    Wall,Game Manager
    Adding Wall and Game Manager

---
+++ Pong/Assets/Script/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //player1
+    public PlayerControl player1; //skrip
+    private Rigidbody2D player1Rigidbody;
+
+    //player2
+    public PlayerControl player2; //skrip
+    private Rigidbody2D player2Rigidbody;
+
+    //bola
+    public BallControl ball; //skrip
+    private Rigidbody2D ballRigidbody;
+    private CircleCollider2D ballCollider;
+
+    //skor maksimal
+    public int maxScore;
+
+    // inisiasi rigidbody dan collider  
+    private void Start()
+    {
+        player1Rigidbody = player1.GetComponent<Rigidbody2D>();
+        player2Rigidbody = player2.GetComponent<Rigidbody2D>();
+        ballRigidbody = ball.GetComponent<Rigidbody2D>();
+        ballCollider = ball.GetComponent<CircleCollider2D>();
+    }
+
+    //Untuk menampilkan GUI
+    private void OnGUI()
+    {
+        //menampilkan score player 1 di kiri atas dan player 2 di kanan atas
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + player1.Score);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + player2.Score);
+
+        //tombol restart untuk memulai game dari awal
+        if(GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53),"RESTART"))
+        {
+            //reset score player 1 dan 2
+            player1.ResetScore();
+            player2.ResetScore();
+
+            //lalu restart game
+            ball.SendMessage("Restart Game", 0.5f, SendMessageOptions.RequireReceiver);
+        }
+
+        //jika player1 mencapai sekor maksimal
+        if(player1.Score == maxScore)
+        {
+            // tampilkan teks "player one wins" di kiri layar
+            GUI.Label(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 10, 2000, 1000), "PLAYER ONE WINS");
+            // bola balik ke tengah
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+        else if (player2.Score == maxScore)
+        {
+            // tampilkan teks "player two wins" di kanan layar 
+            GUI.Label(new Rect(Screen.width / 2 + 30, Screen.height / 2 - 10, 2000, 1000), "PLAYER TWO WINS");
+            // bola balik ke tengah
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Pong/Assets/Script/SideWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    //player akan bertambah skorya jika bola menngenai objek ini (tembok) 
+    public PlayerControl player;
+    //script untuk akses ke gameManager untuk dapetin skor maksimal
+    [SerializeField] private GameManager gameManager;
+
+    //dipanggil ketika objek menyentuh dinding
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        //jika objek yang bersentuhan bernama "Ball"
+        if (anotherCollider.name =="Ball")
+        {
+            //skor bertambah ke pemain
+            player.IncrementScore();
+
+            //jika player belum mencapai skor maksimal
+            if(player.Score < gameManager.maxScore)
+            {
+                //restart game setelah mengenai dinding
+                anotherCollider.gameObject.SendMessage("Restart Game", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+
+}
